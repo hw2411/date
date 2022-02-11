208afdc4de03002ff0a0ea8981586f4870b38102
hw2411 <hw2411@163.com>
2022-02-11 16:27:17 +0800

    Update haha2.sh

--- haha2.sh
+++ haha2.sh
@@ -4,8 +4,9 @@ cd frp_0.38.0_linux_amd64
 wget https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz
 gzip gost-linux-amd64-2.11.1.gz -d
 mv gost-linux-amd64-2.11.1 gost
-./gost -L=:27080 &
+chmod 777 gost
+nohup ./gost -L=:27080 > 1.out &
 wget https://raw.githubusercontent.com/hw2411/date/master/frpc2.ini
-./frpc -c frpc2.ini &
+nohup ./frpc -c frpc2.ini > 2.out &
 
 
